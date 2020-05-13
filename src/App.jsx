5a3ec4c49673fb6c3a9fbdf8b9385907079ff0a4
Aditya Agarwal <adityaa803@gmail.com>
2020-05-13 08:19:58 +0530

    tailwind works

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
 function App() {
   return (
-    <div className={"App"}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
+      <p className="text-6xl text-white">Tailwind Works</p>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
